98a1369f107c012fd5abd61ba547227e1c0f9f7a
IGSebK <etu36796@henallux.be>
2019-12-28 11:13:34 +0100

    proximity fragment: recyclerview Ok, reste à calculer distance entre user et ferme puis trier par km.

---
+++ app/src/main/java/com/example/beetrootapp/adapter/RecyclerViewProximityAdapter.java
@@ -0,0 +1,90 @@
+package com.example.beetrootapp.adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.beetrootapp.R;
+import com.example.beetrootapp.model.Farm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RecyclerViewProximityAdapter extends RecyclerView.Adapter<RecyclerViewProximityAdapter.ViewHolder> {
+    private static final String TAG = "RVProximityAdapter";
+    private ArrayList<Farm> farms;
+    private Context context;
+
+    CircleImageView imageFarm;
+    TextView nameFarm;
+    TextView phoneFarm;
+    TextView km;
+    RatingBar ratingBar;
+    RelativeLayout parentLayout;
+
+    public RecyclerViewProximityAdapter(ArrayList<Farm> farms, Context context) {
+        super();
+        this.farms = farms;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_list_proximity,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG,"onBindViewHolder appelé");
+        imageFarm = (CircleImageView)holder.itemView.findViewById(R.id.image_farm);
+        nameFarm = (TextView)holder.itemView.findViewById(R.id.farm_name);
+        phoneFarm = (TextView)holder.itemView.findViewById(R.id.farm_phone);
+        km = (TextView)holder.itemView.findViewById(R.id.km_proximity);
+        ratingBar = (RatingBar)holder.itemView.findViewById(R.id.rating_bar_farm);
+        parentLayout = holder.itemView.findViewById(R.id.parent_layout_proximity);
+
+//        Glide.with(context).asBitmap().load("https://resultadosdigitais.com.br/blog/files/2018/01/url-amigavel.jpg").into(holder.imageFarm);
+
+
+        phoneFarm.setText(farms.get(position).getUser().getPhone());
+        nameFarm.setText(farms.get(position).getName());
+        km.setText("10km");
+        parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,farms.get(position).getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return farms.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public  View view;
+        public ViewHolder(View proximityView){
+            super(proximityView);
+            view = proximityView;
+        }
+
+
+    }
+}
--- app/src/main/java/com/example/beetrootapp/fragment/ProximityFragment.java
+++ app/src/main/java/com/example/beetrootapp/fragment/ProximityFragment.java
@@ -8,13 +8,50 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.beetrootapp.R;
+import com.example.beetrootapp.ViewModel.FarmVM;
+import com.example.beetrootapp.adapter.RecyclerViewProximityAdapter;
+import com.example.beetrootapp.model.Farm;
+import com.example.beetrootapp.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProximityFragment extends Fragment {
+
+    private ArrayList<Farm> farmsProximity;
+    private FarmVM farmVM;
+    private RecyclerView recyclerView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_proximity,container,false);
+        View view = inflater.inflate(R.layout.fragment_proximity,container,false);
+        recyclerView = (RecyclerView)view.findViewById(R.id.recycler_view_proximity);
+
+        farmsProximity = new ArrayList<>();
+
+        farmVM = ViewModelProviders.of(this).get(FarmVM.class);
+        farmVM.getFarms().observe(this,farms -> {
+            farmsProximity = (ArrayList<Farm>) farms;
+            initRecyclerView();
+
+        });
+        return view;
     }
+
+    private void initRecyclerView(){
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this.getActivity());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        RecyclerViewProximityAdapter recyclerViewProximityAdapter = new RecyclerViewProximityAdapter(farmsProximity,this.getContext());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(recyclerViewProximityAdapter);
+    }
+
+
 }
