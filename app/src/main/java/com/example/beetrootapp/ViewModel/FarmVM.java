8e58a5fe62d7f3c10984fb87b3a373da4e0713aa
Benjamin Francoisse <benjamin.francoisse@gmail.com>
2019-12-29 01:37:45 +0100

    Affichage dynamique des informations dans FarmActivity

--- app/src/main/java/com/example/beetrootapp/ViewModel/FarmVM.java
+++ app/src/main/java/com/example/beetrootapp/ViewModel/FarmVM.java
@@ -11,11 +11,19 @@ import java.util.List;
 public class FarmVM extends androidx.lifecycle.ViewModel{
 
     private LiveData<List<Farm>> farms;
+    private LiveData<Farm> farm;
 
     public LiveData<List<Farm>> getFarms() { //tt
         farms = new MutableLiveData<>();
         farms = loadFarms();
         return farms;
+    }
+
+    public LiveData<Farm> getFarmByUserId(int id) {
+        FarmRepository farmRepository = new FarmRepository();
+        farm = new MutableLiveData<>();
+        farm = farmRepository.getFarmByUserId(id);
+        return farm;
     }
 
     private LiveData<List<Farm>> loadFarms(){
--- app/src/main/java/com/example/beetrootapp/service/FarmService.java
+++ app/src/main/java/com/example/beetrootapp/service/FarmService.java
@@ -6,10 +6,14 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface FarmService {
     String BASE_URL = "https://beetrootapi.azurewebsites.net/api/";
 
     @GET("Farm/getAllFarms")
     Call<List<Farm>> getAllFarms();
+
+    @GET("Farm/getByUserId")
+    Call<Farm> getFarmByUserId(@Query("id")Integer id);
 }
